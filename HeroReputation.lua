ecea06743929f9dd80c3b8f8688d7a647d701b20
Raphaël Bonnet <harcyah@gmail.com>
2020-11-20 09:03:26 +0100

    Use single quotes around strings

--- HeroReputation.lua
+++ HeroReputation.lua
@@ -1,36 +1,36 @@
 
-local frame = CreateFrame("Frame");
-frame:RegisterEvent("ZONE_CHANGED_NEW_AREA");
-frame:RegisterEvent("PLAYER_ENTERING_WORLD");
+local frame = CreateFrame('Frame');
+frame:RegisterEvent('ZONE_CHANGED_NEW_AREA');
+frame:RegisterEvent('PLAYER_ENTERING_WORLD');
 
 local zoneReputationMap = {
 	-- BFA
-	["Rade de Tiragarde"] = "Amirauté des Portvaillant",
-	["Vallée Chantorage"] = "Sillage des tempêtes",
-	["Drustvar"] = "Ordre des Braises",
-	["Vol’dun"] = "7e Légion",
-	["Nazmir"] = "7e Légion",
-	["Zuldazar"] = "7e Légion",
-	["Nazjatar"] = "Ankoïens du Brisant",
-	["Île de Mécagone"] = "Résistance de Mécarouille",
+	['Rade de Tiragarde'] = 'Amirauté des Portvaillant',
+	['Vallée Chantorage'] = 'Sillage des tempêtes',
+	['Drustvar'] = 'Ordre des Braises',
+	['Vol’dun'] = '7e Légion',
+	['Nazmir'] = '7e Légion',
+	['Zuldazar'] = '7e Légion',
+	['Nazjatar'] = 'Ankoïens du Brisant',
+	['Île de Mécagone'] = 'Résistance de Mécarouille',
 
 	-- BFA assaults
-	["Uldum"] = "Accord d’Uldum",
-	["Val de l’Éternel printemps"] = "Rajani",
+	['Uldum'] = 'Accord d’Uldum',
+	['Val de l’Éternel printemps'] = 'Rajani',
 
 	-- Legion
-	["Tornheim"] = "Valarjar",
-	["Haut-Roc"] = "Tribu de Haut-Roc",
-	["Val’sharah"] = "Tisse-Rêves",
-	["Suramar"] = "Souffrenuit",
-	["Azsuna"] = "Cour de Farondis",
+	['Tornheim'] = 'Valarjar',
+	['Haut-Roc'] = 'Tribu de Haut-Roc',
+	['Val’sharah'] = 'Tisse-Rêves',
+	['Suramar'] = 'Souffrenuit',
+	['Azsuna'] = 'Cour de Farondis',
 
 	-- Cataclysm
-	["Hautes-terres du Crépuscule"] = "Clan Marteau-Hardi",
+	['Hautes-terres du Crépuscule'] = 'Clan Marteau-Hardi',
 }
 
 local function onZoneChange()
-	if (UnitLevel("player") < 50) then
+	if (UnitLevel('player') < 50) then
 		return
 	end
 
@@ -49,14 +49,14 @@ local function onZoneChange()
 	SetWatchedFactionIndex(0)
 end
 
-frame:SetScript("OnEvent", function(self, event, ...)
+frame:SetScript('OnEvent', function(self, event, ...)
 	local arg = {...}
 
-	if (event == "ZONE_CHANGED_NEW_AREA") then
+	if (event == 'ZONE_CHANGED_NEW_AREA') then
 		onZoneChange()
 	end
 
-	if (event == "PLAYER_ENTERING_WORLD") then
+	if (event == 'PLAYER_ENTERING_WORLD') then
 		onZoneChange()
 	end
 
